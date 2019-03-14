6df1a7af1e91a78b4a368581cd5019e889bb1c44
720694-Ogle <720694-Ogle@WISSEN>
2019-03-14 18:56:42 +0300

    Şifre formatı gereklilikleri değiştirildi.

--- NeYapsak.PL/App_Start/IdentityConfig.cs
+++ NeYapsak.PL/App_Start/IdentityConfig.cs
@@ -37,11 +37,11 @@ namespace NeYapsak.PL.App_Start
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequiredLength = 8,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
