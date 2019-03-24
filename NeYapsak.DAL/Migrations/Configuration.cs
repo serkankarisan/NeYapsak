5741e5feabaa369b92706dd6e360ecf0a739fc26
Serkan <Serkan@GS-1905>
2019-03-25 02:38:07 +0300

    Ufak görsel düzenlemeler ve sayfa title'ları yapıldı.

--- NeYapsak.DAL/Migrations/Configuration.cs
+++ NeYapsak.DAL/Migrations/Configuration.cs
@@ -19,15 +19,18 @@ namespace NeYapsak.DAL.Migrations
 
         protected override void Seed(NeYapsak.DAL.Context.NeYapsakContext context)
         {
+            //DateTime dtarihi = Convert.ToDateTime("19.10.1996");
 
             //IdentityUserRole UserRole;
 
             //ApplicationRole ApRoleAdmin = new ApplicationRole() { Id = "5e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "Admin", Description = "Yönetici" };
             //ApplicationRole ApRoleUser = new ApplicationRole() { Id = "3e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "User", Description = "Kullanýcý" };
 
-            //ApplicationUser ApUserAdmin = new ApplicationUser() { Id = "6e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "Mehmet ali", Surname = "Coþar", Email = "memo@gmail.com", UserName = "mehmetcosar", DogumTarihi = DateTime.Now, EmailConfirmed = true, PasswordHash = "ABjmuPvN9Zaw/+7SM5GYHUZXWDahYkc48tBpmNNlVVgM5Rj02N9rVzeMzPGAlzJeXA==", SecurityStamp = "d6b253a0-6325-410b-bed4-796ba916e443", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };//burada ýd,password ve securitystamp deðiþecek.
-            //ApplicationUser ApUserUser1 = new ApplicationUser() { Id = "90fd7acd-9a43-488b-80bb-d6084507c441", Name = "Oya", Surname = "Koþar", Email = "oya@gmail.com", UserName = "oyakosar", DogumTarihi = DateTime.Now, EmailConfirmed = true, PasswordHash = "AJLZWCBM0sIffqRzoPgim0OV1tomiWFDAG/6r/ARWA9IbTc4vF+9r8CuZSEBpvwTsg==", SecurityStamp = "f67c8d73-6d11-4e4f-b9fc-4cfbb6a4dadc", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };
-            //ApplicationUser ApUserUser2 = new ApplicationUser() { Id = "7e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "Ali", Surname = "Uçar", Email = "ali@gmail.com", UserName = "aliucar", DogumTarihi = DateTime.Now, EmailConfirmed = false, PasswordHash = "ABjmuPvN9Zaw/+7SM5GYHUZXWDahYkc48tBpmNNlVVgM5Rj02N9rVzeMzPGAlzJeXA==", SecurityStamp = "d6b253a0-6325-410b-bed4-796ba916e443", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };
+            //ApplicationUser ApUserAdmin = new ApplicationUser() { Id = "6e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "Serkan", Surname = "Karýþan", Email = "serkankarisan1905@gmail.com", UserName = "serkankarisan1905@gmail.com", DogumTarihi = dtarihi, EmailConfirmed = true, PasswordHash = "ABjmuPvN9Zaw/+7SM5GYHUZXWDahYkc48tBpmNNlVVgM5Rj02N9rVzeMzPGAlzJeXA==", SecurityStamp = "d6b253a0-6325-410b-bed4-796ba916e443", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };
+
+            //ApplicationUser ApUserUser1 = new ApplicationUser() { Id = "90fd7acd-9a43-488b-80bb-d6084507c441", Name = "Wesley", Surname = "Sneijder", Email = "weseley37@gmail.com", UserName = "weseley37@gmail.com", DogumTarihi = dtarihi, EmailConfirmed = false, PasswordHash = "AJLZWCBM0sIffqRzoPgim0OV1tomiWFDAG/6r/ARWA9IbTc4vF+9r8CuZSEBpvwTsg==", SecurityStamp = "f67c8d73-6d11-4e4f-b9fc-4cfbb6a4dadc", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };
+
+            //ApplicationUser ApUserUser2 = new ApplicationUser() { Id = "7e8edacb-d8f9-47b3-91f1-028643139e1d", Name = "Ali", Surname = "Uçar", Email = "karisanserkan@gmail.com", UserName = "karisanserkan@gmail.com", DogumTarihi = dtarihi, EmailConfirmed = true, PasswordHash = "ABjmuPvN9Zaw/+7SM5GYHUZXWDahYkc48tBpmNNlVVgM5Rj02N9rVzeMzPGAlzJeXA==", SecurityStamp = "d6b253a0-6325-410b-bed4-796ba916e443", PhoneNumber = null, PhoneNumberConfirmed = false, TwoFactorEnabled = false, LockoutEndDateUtc = null, LockoutEnabled = false, AccessFailedCount = 0 };
 
 
             //UserRole = new IdentityUserRole() { RoleId = "5e8edacb-d8f9-47b3-91f1-028643139e1d", UserId = "6e8edacb-d8f9-47b3-91f1-028643139e1d" };
@@ -57,25 +60,27 @@ namespace NeYapsak.DAL.Migrations
             //    );
 
             //context.Etiketler.AddOrUpdate(
-            // new Etiket { EtiketAdi = "Halý saha", Aciklama = "Maç mý yapsak", Silindi = false },
-            // new Etiket { EtiketAdi = "Batak", Aciklama = "Batak mý yapsak", Silindi = false },
-            // new Etiket { EtiketAdi = "101", Aciklama = "101 mi yapsak", Silindi = false }
+            // new Etiket { EtiketAdi = "Halý saha", Aciklama = "Maç", Silindi = false },
+            // new Etiket { EtiketAdi = "Batak", Aciklama = "Batak", Silindi = false },
+            // new Etiket { EtiketAdi = "101", Aciklama = "101", Silindi = false },
+            // new Etiket { EtiketAdi = "Sinema", Aciklama = "Sinema", Silindi = false },
+            // new Etiket { EtiketAdi = "Gezi", Aciklama = "Gezi", Silindi = false },
+            // new Etiket { EtiketAdi = "PS", Aciklama = "Playstation", Silindi = false },
+            // new Etiket { EtiketAdi = "CS", Aciklama = "CounterStrike", Silindi = false },
+            // new Etiket { EtiketAdi = "Eðlence", Aciklama = "Eðlence", Silindi = false }
             //);
 
             //context.Ilanlar.AddOrUpdate(
-            //new Ilan { Id = 1, Baslik = "Halýsaha Maçý mý yapsak.", BaslangicTarihi = DateTime.Now, GoruntulenmeSayaci = 1, Il = "Ýstanbul", Ilce = "Beþiktaþ", Kontenjan = 2, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", OlusturmaTarihi = DateTime.Now, Ozet = "Hele  gelde maç yapak.kaleye adam lazým :).loyloy.", Silindi = false, Yayindami = true }
-            //);
-
-            //context.IlanHareketler.AddOrUpdate(
-            //new IlanHareket { IlanId = 1, IlgilenenId = 1, KatilanId = 0, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", Tarih = DateTime.Now, Silindi = false }
+            //new Ilan { Id = 1, Baslik = "Halýsaha Maçý mý yapsak.", BaslangicTarihi = DateTime.Now, GoruntulenmeSayaci = 0, Il = "Ýstanbul", Ilce = "Beþiktaþ", Kontenjan = 2, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", OlusturmaTarihi = DateTime.Now, Ozet = "Hele  gelde maç yapak.kaleye adam lazým :).", Silindi = false, Yayindami = true },
+            //new Ilan { Id = 2, Baslik = "Pes Oynamaya Yoldaþ Lazým :)", BaslangicTarihi = DateTime.Now, GoruntulenmeSayaci = 0, Il = "Ýstanbul", Ilce = "Üsküdar", Kontenjan = 1, KullaniciId = "6e8edacb-d8f9-47b3-91f1-028643139e1d", OlusturmaTarihi = DateTime.Now, Ozet = "Kendine güvenen gelsin.", Silindi = false, Yayindami = true }
             //);
 
             //context.Katilanlar.AddOrUpdate(
-            //  new Katilan { Id = 1, IlanId = 1, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", Tarih = DateTime.Now, Silindi = false }
+            //  new Katilan { Id = 1, IlanId = 1, KullaniciId = "7e8edacb-d8f9-47b3-91f1-028643139e1d", Tarih = DateTime.Now, Silindi = false }
             //);
 
             //context.SikayetVeOneriler.AddOrUpdate(
-            //  new SikayetVeOneri { Aciklama = "kendi ilanýmdan þikayetçiyim.hele.hele.", Id = 1, IlanId = 1, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", Tarih = DateTime.Now, Silindi = false }
+            //  new SikayetVeOneri { Aciklama = "Kendi ilanýmdan þikayetçiyim.", Id = 1, IlanId = 1, KullaniciId = "90fd7acd-9a43-488b-80bb-d6084507c441", Tarih = DateTime.Now, Silindi = false }
             //);
         }
     }
